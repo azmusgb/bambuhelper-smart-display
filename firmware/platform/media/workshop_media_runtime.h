44b1f16be9c3c7b4eda8edc59a9d09732da659c1
azmusgb <36898133+azmusgb@users.noreply.github.com>
2026-09-18 06:13:08 -0400

    feat(os12): expose video diagnostics from media runtime

--- firmware/platform/media/workshop_media_runtime.h
+++ firmware/platform/media/workshop_media_runtime.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include "media_service.h"
+#include "ws350_media_backend.h"
 
 void workshopMediaBegin();
 void workshopMediaPoll();
 workshop::media::MediaService& workshopMediaService();
 const workshop::media::Snapshot& workshopMediaSnapshot();
+const workshop::media::VideoDiagnostics& workshopVideoDiagnostics();
